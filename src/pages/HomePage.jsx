98a98971dfba9c8452f9b949fc5995483401be70
Javier Roca <javierroca.dev@gmail.com>
2023-12-30 11:55:53 +0100

    prueba

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold">¡Bienvenido a mi aplicación!</h1>
-      <h1 className="text-3xl font-bold">¡Bldaksddkkdkd</h1>
+      dlsls
       <h1 className="text-3xl font-bold">¡Bienvenido a mi aplicación!</h1>
       {/* Otro contenido específico de la página */}
     </div>
