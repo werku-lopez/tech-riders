7e981ca03038ed43132eae874f07b9066d284ddc
Javier Roca <javierroca.dev@gmail.com>
2023-12-31 03:09:58 +0100

    home y contact creado, added tailwind config

---
+++ src/assets/icons/icons.jsx
@@ -0,0 +1,138 @@
+//https://www.svgrepo.com/collection/untitled-ui-oval-interface-icons
+import React from "react";
+
+export const User = ({
+  size = 24,
+  color = "currentColor",
+  className = "",
+  ...props
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={`${className}`}
+    {...props}
+  >
+    <path
+      d="M20 21C20 19.6044 20 18.9067 19.8278 18.3389C19.44 17.0605 18.4395 16.06 17.1611 15.6722C16.5933 15.5 15.8956 15.5 14.5 15.5H9.5C8.10444 15.5 7.40665 15.5 6.83886 15.6722C5.56045 16.06 4.56004 17.0605 4.17224 18.3389C4 18.9067 4 19.6044 4 21M16.5 7.5C16.5 9.98528 14.4853 12 12 12C9.51472 12 7.5 9.98528 7.5 7.5C7.5 5.01472 9.51472 3 12 3C14.4853 3 16.5 5.01472 16.5 7.5Z"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>{" "}
+  </svg>
+);
+export const Bookmark = ({
+  size = 24,
+  color = "currentColor",
+  className = "",
+  ...props
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={`${className}`}
+    {...props}
+  >
+    <path
+      d="M5 7.8C5 6.11984 5 5.27976 5.32698 4.63803C5.6146 4.07354 6.07354 3.6146 6.63803 3.32698C7.27976 3 8.11984 3 9.8 3H14.2C15.8802 3 16.7202 3 17.362 3.32698C17.9265 3.6146 18.3854 4.07354 18.673 4.63803C19 5.27976 19 6.11984 19 7.8V21L12 17L5 21V7.8Z"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>{" "}
+  </svg>
+);
+export const Message = ({
+  size = 24,
+  color = "currentColor",
+  className = "",
+  ...props
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={`${className}`}
+    {...props}
+  >
+    <path
+      d="M7.5 10.5H7.51M12 10.5H12.01M16.5 10.5H16.51M9.9 19.2L11.36 21.1467C11.5771 21.4362 11.6857 21.5809 11.8188 21.6327C11.9353 21.678 12.0647 21.678 12.1812 21.6327C12.3143 21.5809 12.4229 21.4362 12.64 21.1467L14.1 19.2C14.3931 18.8091 14.5397 18.6137 14.7185 18.4645C14.9569 18.2656 15.2383 18.1248 15.5405 18.0535C15.7671 18 16.0114 18 16.5 18C17.8978 18 18.5967 18 19.1481 17.7716C19.8831 17.4672 20.4672 16.8831 20.7716 16.1481C21 15.5967 21 14.8978 21 13.5V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V13.5C3 14.8978 3 15.5967 3.22836 16.1481C3.53284 16.8831 4.11687 17.4672 4.85195 17.7716C5.40326 18 6.10218 18 7.5 18C7.98858 18 8.23287 18 8.45951 18.0535C8.76169 18.1248 9.04312 18.2656 9.2815 18.4645C9.46028 18.6137 9.60685 18.8091 9.9 19.2ZM8 10.5C8 10.7761 7.77614 11 7.5 11C7.22386 11 7 10.7761 7 10.5C7 10.2239 7.22386 10 7.5 10C7.77614 10 8 10.2239 8 10.5ZM12.5 10.5C12.5 10.7761 12.2761 11 12 11C11.7239 11 11.5 10.7761 11.5 10.5C11.5 10.2239 11.7239 10 12 10C12.2761 10 12.5 10.2239 12.5 10.5ZM17 10.5C17 10.7761 16.7761 11 16.5 11C16.2239 11 16 10.7761 16 10.5C16 10.2239 16.2239 10 16.5 10C16.7761 10 17 10.2239 17 10.5Z"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>{" "}
+  </svg>
+);
+export const Pantalla = ({
+  size = 24,
+  color = "currentColor",
+  className = "",
+  ...props
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={`${className}`}
+    {...props}
+  >
+    <path
+      d="M9 17L4 22M15 17L20 22M12 2V4M12 22V17M5.2 17H18.8C19.9201 17 20.4802 17 20.908 16.782C21.2843 16.5903 21.5903 16.2843 21.782 15.908C22 15.4802 22 14.9201 22 13.8V7.2C22 6.07989 22 5.51984 21.782 5.09202C21.5903 4.71569 21.2843 4.40973 20.908 4.21799C20.4802 4 19.9201 4 18.8 4H5.2C4.07989 4 3.51984 4 3.09202 4.21799C2.71569 4.40973 2.40973 4.71569 2.21799 5.09202C2 5.51984 2 6.07989 2 7.2V13.8C2 14.9201 2 15.4802 2.21799 15.908C2.40973 16.2843 2.71569 16.5903 3.09202 16.782C3.51984 17 4.0799 17 5.2 17Z"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>{" "}
+  </svg>
+);
+export const Presencial = ({
+  size = 24,
+  color = "currentColor",
+  className = "",
+  ...props
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={`${className}`}
+    {...props}
+  >
+    <path
+      d="M15 21V15.6C15 15.0399 15 14.7599 14.891 14.546C14.7951 14.3578 14.6422 14.2049 14.454 14.109C14.2401 14 13.9601 14 13.4 14H10.6C10.0399 14 9.75992 14 9.54601 14.109C9.35785 14.2049 9.20487 14.3578 9.10899 14.546C9 14.7599 9 15.0399 9 15.6V21M19 21V6.2C19 5.0799 19 4.51984 18.782 4.09202C18.5903 3.71569 18.2843 3.40973 17.908 3.21799C17.4802 3 16.9201 3 15.8 3H8.2C7.07989 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V21M21 21H3M9.5 8H9.51M14.5 8H14.51M10 8C10 8.27614 9.77614 8.5 9.5 8.5C9.22386 8.5 9 8.27614 9 8C9 7.72386 9.22386 7.5 9.5 7.5C9.77614 7.5 10 7.72386 10 8ZM15 8C15 8.27614 14.7761 8.5 14.5 8.5C14.2239 8.5 14 8.27614 14 8C14 7.72386 14.2239 7.5 14.5 7.5C14.7761 7.5 15 7.72386 15 8Z"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>{" "}
+  </svg>
+);
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,190 @@
-import React from 'react';
+import React from "react";
+import heroImg from "../assets/images/HeroImg.jpeg";
+import logo from "../assets/images/LogoTechRiders.png";
+import logoTajamar from "../assets/images/LogoTajamar.png";
+import galery1 from "../assets/images/galery1.jpeg"
+import galery2 from "../assets/images/galery2.jpeg"
+import galery3 from "../assets/images/galery3.jpeg"
+import galery4 from "../assets/images/galery4.jpeg"
+import galery5 from "../assets/images/galery5.jpeg"
+import galery6 from "../assets/images/galery6.jpg"
 
+import {User, Bookmark, Message, Pantalla, Presencial} from "../assets/icons/icons"
 const HomePage = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold">¡Bienvenido a mi aplicación!</h1>
-      dlsls
-      <h1 className="text-3xl font-bold">¡Bienvenido a mi aplicación!</h1>
-      {/* Otro contenido específico de la página */}
+    <>
+      <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 class="title-font text-7xl  mb-4 font-bold text-gray-900">
+              Tech Riders
+            </h1>
+
+            <p class="mb-8 leading-relaxed text-xl ">
+              Conectando Futuros: Charlas Tecnológicas para la Excelencia en la
+              Formación Profesional.
+            </p>
+            <div class="flex justify-center">
+              <button class="inline-flex text-white bg-primary-500 border-0 py-2 px-6 focus:outline-none hover:bg-primary-600 rounded text-lg">
+                Log in
+              </button>
+              <button class="ml-4 inline-flex text-gray-700 border-2 border-primary-500 bg-neutral-100  py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                Sobre nosotros
+              </button>
+            </div>
+          </div>
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 relative ">
+            <img
+              class="object-cover object-center rounded"
+              alt="hero"
+              src={heroImg}
+            />
+            <img
+              class="object-cover object-center rounded absolute -bottom-12 -right-12 [filter:_invert(50%);] "
+              alt="hero"
+              src={logoTajamar}
+            />
+          </div>
+        </div>
+      </section>
+
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 gap-24 mx-auto">
+          <div class="flex flex-wrap w-full mb-12 flex-col items-center text-center">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+              Charlas
+            </h1>
+            <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
+              Descubre los beneficios de añadir esta dinámica a tu centro.
+            </p>
+          </div>
+          <div class="flex flex-wrap justify-center -m-4">
+            <div class="xl:w-1/3 md:w-1/2  p-4">
+              <div class="border border-gray-200 min-h-[280px]  p-6 rounded-lg">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-primary-100 text-primary-500 mb-4">
+                  <User className="text-primary-500"/>
+                </div>
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                  Inspirar y motivar
+                </h2>
+                <p class="leading-relaxed text-base">
+                  Inspiraramos a los estudiantes al mostrarles casos de éxito,
+                  historias de superación y la emocionante gama de posibilidades
+                  que ofrece el mundo tecnológico.{" "}
+                </p>
+              </div>
+            </div>
+            <div class="xl:w-1/3 md:w-1/2  p-4">
+              <div class="border border-gray-200 min-h-[280px] p-6 rounded-lg">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-primary-100 text-primary-500 mb-4">
+                  <Bookmark className="text-primary-500"/>
+                </div>
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                  Demostrar aplicaciones prácticas
+                </h2>
+                <p class="leading-relaxed text-base">
+                  A través de ejemplos y demostraciones prácticas, los oradores
+                  pueden ilustrar la aplicación real de conceptos tecnológicos.
+                </p>
+              </div>
+            </div>
+            <div class="xl:w-1/3 md:w-1/2  p-4">
+              <div class="border border-gray-200 min-h-[280px] p-6 rounded-lg">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-primary-100 text-primary-500 mb-4">
+                  <Message className=""/>
+                </div>
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                  Conexiones Profesionales
+                </h2>
+                <p class="leading-relaxed text-base">
+                  Dar charlas en centros de formación profesional brinda la
+                  oportunidad de establecer conexiones con estudiantes,
+                  profesores y otros profesionales del sector.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap w-full mb-12 flex-col items-center text-center">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+              Modalidades
+            </h1>
+            <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
+              Nos adaptamos a las necesidades de tu centro
+            </p>
+          </div>
+          <div class="flex flex-wrap justify-center -m-4">
+   
+            <div class="xl:w-1/3 md:w-1/2  p-4">
+              <div class="border border-gray-200 min-h-[280px] p-6 rounded-lg">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-primary-100 text-primary-500 mb-4">
+                  <Presencial/>
+                </div>
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                  Presencial
+                </h2>
+                <p class="leading-relaxed text-base">
+                Inspiraramos a los estudiantes al mostrarles casos de éxito, historias de superación y la emocionante gama de posibilidades que ofrece el mundo tecnológico. </p>
+              </div>
+            </div>
+            <div class="xl:w-1/3 md:w-1/2  p-4">
+              <div class="border border-gray-200 min-h-[280px] p-6 rounded-lg">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-primary-100 text-primary-500 mb-4">
+                  <Pantalla/>
+                </div>
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                  Híbrido
+                </h2>
+                <p class="leading-relaxed text-base">
+                Inspiraramos a los estudiantes al mostrarles casos de éxito, historias de superación y la emocionante gama de posibilidades que ofrece el mundo tecnológico. 
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="text-gray-600 body-font">
+  <div class="container px-5 gap-12 mx-auto flex flex-wrap">
+
+    <div class="flex flex-wrap md:-m-2 -m-1">
+      <div class="flex flex-wrap w-1/2">
+        <div class="md:p-2 p-1 w-1/2">
+          <img alt="gallery" class="w-full rounded object-cover h-full object-center block" src={galery1}/>
+        </div>
+        <div class="md:p-2 p-1 w-1/2">
+          <img alt="gallery" class="w-full rounded object-cover h-full object-center block" src={galery4}/>
+        </div>
+        <div class="md:p-2 p-1 w-full">
+          <img alt="gallery" class="w-full rounded h-full object-cover object-center block" src={galery2}/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/2">
+        <div class="md:p-2 p-1 w-full">
+          <img alt="gallery" class="w-full rounded h-full object-cover object-center block" src={galery3}/>
+        </div>
+        <div class="md:p-2 p-1 w-1/2">
+          <img alt="gallery" class="w-full rounded object-cover h-full object-center block" src={galery5}/>
+        </div>
+        <div class="md:p-2 p-1 w-1/2">
+          <img alt="gallery" class="w-full rounded object-cover h-full object-center block" src={galery6}/>
+        </div>
+      </div>
     </div>
+    <div class="flex w-full my-12 flex-col items-center text-center gap-4">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900  ">¿A qué estas esperando?</h1>
+      <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">No te pierdas la oportunidad de sumergirte en el emocionante universo de la tecnología. ¡Explora las charlas disponibles y si te gusta alguna no dudes en solicitarla. Aprender, inspirarnos y construir el futuro juntos es posible</p>
+      <button class="inline-flex text-white bg-primary-500 border-0 py-2 px-6 focus:outline-none hover:bg-primary-600 rounded text-lg">
+                ¡Apuntate ahora!
+              </button>
+    </div>
+  </div>
+</section>
+    </>
   );
 };
 
